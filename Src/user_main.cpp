598665419303c0e200c7c89544313c03bfe05074
SuWeipeng <weipeng.su@outlook.com>
2019-08-25 22:14:18 +0800

    调试左前轮 OK

--- Src/user_main.cpp
+++ Src/user_main.cpp
@@ -1,8 +1,8 @@
 #include <AP_Motors.h>
 #include <AC_PID.h>
 
-AC_PID    *pid_1;
-AP_Motors *motor1_fr;
+AC_PID    *pid_1, *pid_2;
+AP_Motors *motor1_fr, *motor2_fl;
 
 extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim3;
@@ -16,6 +16,7 @@ extern "C" {
 void setup(void)
 {
   pid_1     = new AC_PID(1.2f, 1.2f, 0.0f, 0.0f, 95.0f, 1.0f, 1.0f, 0.85f, 0.02f);
+  pid_2     = new AC_PID(1.2f, 1.2f, 0.0f, 0.0f, 95.0f, 1.0f, 1.0f, 0.85f, 0.02f);
   motor1_fr = new AP_Motors(&htim3,        // encoder timer
                             1,             // encoder direction
                             GPIOC,         // L298N GPIO port
@@ -25,11 +26,21 @@ void setup(void)
                             TIM_CHANNEL_4, // pwm channel
                             99,
                             pid_1);
+  motor2_fl = new AP_Motors(&htim1,        // encoder timer
+                            1,             // encoder direction
+                            GPIOE,         // L298N GPIO port
+                            GPIO_PIN_2,    // L298N in1
+                            GPIO_PIN_4,    // L298N in2
+                            &htim5,        // pwm timer
+                            TIM_CHANNEL_1, // pwm channel
+                            99,
+                            pid_2);
 }
 
 void loop(void)
 {
-  motor1_fr -> set_rpm(50.0f);
+  //motor1_fr -> set_rpm(50.0f);
+  motor2_fl -> set_rpm(50.0f);
 }
 
 #if (defined(__GNUC__) || defined(__CC_ARM)) && defined(__cplusplus)
