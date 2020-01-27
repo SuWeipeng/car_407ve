d4a937ed6dfd990b18e9f34f68e53565d52e656b
ubuntu <ubuntu@ubuntu.com>
2020-01-27 16:13:27 +0800

    补充创建 loop() 线程的文件。

---
+++ Src-rtt/thread_loop.c
@@ -0,0 +1,13 @@
+#if defined(USE_RTTHREAD)
+#include <entry.h>
+
+rt_thread_t loop_thread = RT_NULL;
+
+void loop (void* parameter);
+
+int loop_start(void)
+{
+  RTT_CREATE(loop,loop,RT_NULL,2048,5,20);
+}
+
+#endif
