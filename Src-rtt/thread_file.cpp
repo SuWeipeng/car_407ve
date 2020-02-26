6dd5ae7eef6b792a11154dbf63feff8150bcbce5
SuWeipeng <weipeng.su@outlook.com>
2020-02-26 16:48:49 +0800

    降低写 sd 卡线程的优先级，减小调用频率，每次最大写入 sector 不超过 512。

--- Src-rtt/thread_file.cpp
+++ Src-rtt/thread_file.cpp
@@ -4,7 +4,8 @@
 #include "AP_Buffer.h"
 #include "AC_Base.h"
 
-#define LOG_FILE_NAME "log.bin"
+#define LOG_FILE_NAME   "log.bin"
+#define MAX_SECTOR_SIZE 512
 
 FATFS fs;
 FATFS *pfs;
@@ -43,7 +44,7 @@ int file_start(void)
     return 2;
   }
   
-  RTT_CREATE(file,file_log_entry,RT_NULL,1024,1,20);
+  RTT_CREATE(file,file_log_entry,RT_NULL,4096,RT_THREAD_PRIORITY_MAX-2,20);
   return 0;
 }
 
@@ -52,13 +53,13 @@ void file_log_entry (void* parameter){
   return;
 #endif
   uint16_t read_size;
+  uint8_t* pos;
   while(1){
     base->take_sem();
     rt_enter_critical();
     /* Mount SD Card */
     if(f_mount(&fs, "", 0) != FR_OK){
       rt_kputs("f_mount error\r\n");
-      return;
     }
     
     /* Open file to write */
@@ -68,8 +69,7 @@ void file_log_entry (void* parameter){
     
     /* Check freeSpace space */
     if(f_getfree("", &fre_clust, &pfs) != FR_OK){
-      rt_kputs("f_getfree error\r\n");    
-      return;
+      rt_kputs("f_getfree error\r\n");
     }
     
     totalSpace = (uint32_t)((pfs->n_fatent - 2) * pfs->csize * 0.5);
@@ -77,31 +77,41 @@ void file_log_entry (void* parameter){
     
     /* free space is less than 1kb */
     if(freeSpace < 1){
-      rt_kputs("freeSpace error\r\n");    
-      return;
+      rt_kputs("freeSpace error\r\n");
     }
     
     /* Writing*/
     read_size = buffer->read();
     if(read_size>0){
-      f_write (&fil, buffer->read_buf_addr(), read_size, &bw);
+      if(read_size <= MAX_SECTOR_SIZE){
+        f_write (&fil, buffer->read_buf_addr(), read_size, &bw);
+      } else {
+        pos = (uint8_t*)buffer->read_buf_addr();
+        do{
+          f_write (&fil, pos, MAX_SECTOR_SIZE, &bw);
+          pos       += MAX_SECTOR_SIZE;
+          read_size -= MAX_SECTOR_SIZE;
+          if(read_size < MAX_SECTOR_SIZE && read_size > 0){
+            f_write (&fil, pos, read_size, &bw);
+            break;
+          }
+        }while(1);
+      }
     }
     
     /* Close file */
     if(f_close(&fil) != FR_OK){
-      rt_kputs("f_close error\r\n");    
-      return;
+      rt_kputs("f_close error\r\n"); 
     }
     
     /* Unmount SDCARD */
     if(f_mount(NULL, "", 1) != FR_OK){
-      rt_kputs("unmount error\r\n");   
-      return;
+      rt_kputs("unmount error\r\n");
     }
     
     rt_exit_critical();
     
-    rt_thread_delay(1);
+    rt_thread_delay(1000);
   }
 }
 
